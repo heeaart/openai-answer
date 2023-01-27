0afd001a9127eb37acf9e6c8411143e553242a5e
aliha <ali.hatbi@wp.pl>
2023-01-27 16:41:09 +0100

    dodanie kolorkow i mozliwosc kontynuowania

--- chatgpt.py
+++ chatgpt.py
@@ -1,20 +1,29 @@
 import openai
+from colorama import init as colorama_init
+from colorama import Fore
+
+colorama_init()
 
 # Ustawienie klucza API https://beta.openai.com/account/api-keys
-openai.api_key = "twoj-klucz-api"
+openai.api_key = "twoj-kluz-api"
 
-# Twoje pytanie
-print("Zadaj mi pytanie ")
-pytanie = input()
+# Twoje zapytanie
+def odp(wiadomosc):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=wiadomosc,
+        temperature=0.5,
+        max_tokens=1024
+    )
 
-# Wysłanie requesta
-response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=f"{pytanie}",
-    max_tokens=1024
-)
+    return response['choices'][0]['text']
 
-# Wyświetlenie odpowiedzi
-print(response["choices"][0]["text"])
-
+# Rozmowa z openai
+while True:
+    wiadomosc = input(f"{Fore.WHITE}👨‍💻 Ty: ")
+    if wiadomosc == "Narka":
+        print(f"{Fore.RED}👋 Elo do zobaczenia")
+        break
+    else:
+        print(f"{Fore.BLUE}🤖 Bot: {odp(wiadomosc)} {Fore.BLACK} \n{Fore.YELLOW}⚠️Aby zakończyć napisz `Narka`")
 
